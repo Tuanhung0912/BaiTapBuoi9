f81eb7d6faf41aad7cc4d4cdcd59ea7179c9380a
tuanhung0912 <tuanhung.bh2003@gmail.com>
2025-04-03 14:35:08 +0700

    Slug in index.js #2

--- routes/categories.js
+++ routes/categories.js
@@ -31,30 +31,6 @@ router.get('/:id', async function(req, res, next) {
   }
 });
 
-router.get('/slug/:categorySlug', async function(req, res, next) {
-  try {
-      let category = await categoryModel.findOne({ slug: req.params.categorySlug });
-
-      if (!category) {
-          return res.status(404).send({
-              success: false,
-              message: 'Category not found'
-          });
-      }
-
-      let products = await productModel.find({ category: category._id, isDeleted: false });
-
-      res.status(200).send({
-          success: true,
-          data: products
-      });
-  } catch (error) {
-      res.status(500).send({
-          success: false,
-          message: error.message
-      });
-  }
-});
 
 router.post('/', check_authentication, check_authorization(['mod']), async function(req, res) {
   try {
--- routes/products.js
+++ routes/products.js
@@ -59,41 +59,7 @@ router.get('/:id', async function(req, res, next) {
   }
 });
 
-router.get('/slug/:categorySlug/:productSlug', async function(req, res, next) {
-  try {
-      let category = await categoryModel.findOne({ slug: req.params.categorySlug });
 
-      if (!category) {
-          return res.status(404).send({
-              success: false,
-              message: 'Category not found'
-          });
-      }
-
-      let product = await productModel.findOne({ 
-          slug: req.params.productSlug, 
-          category: category._id,
-          isDeleted: false 
-      });
-
-      if (!product) {
-          return res.status(404).send({
-              success: false,
-              message: 'Product not found'
-          });
-      }
-
-      res.status(200).send({
-          success: true,
-          data: product
-      });
-  } catch (error) {
-      res.status(500).send({
-          success: false,
-          message: error.message
-      });
-  }
-});
 
 
 
