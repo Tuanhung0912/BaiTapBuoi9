f9cb1057765588dfc9eacd0df22fc2d495fce427
tuanhung0912 <tuanhung.bh2003@gmail.com>
2025-04-03 14:34:22 +0700

    Slug in index.js

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,65 @@ router.get('/', function(req, res, next) {
   );
 });
 
+router.get('/slug/:categorySlug', async function(req, res, next) {
+  try {
+      let category = await categoryModel.findOne({ slug: req.params.categorySlug });
+
+      if (!category) {
+          return res.status(404).send({
+              success: false,
+              message: 'Category not found'
+          });
+      }
+
+      let products = await productModel.find({ category: category._id, isDeleted: false });
+
+      res.status(200).send({
+          success: true,
+          data: products
+      });
+  } catch (error) {
+      res.status(500).send({
+          success: false,
+          message: error.message
+      });
+  }
+});
+
+router.get('/slug/:categorySlug/:productSlug', async function(req, res, next) {
+  try {
+      let category = await categoryModel.findOne({ slug: req.params.categorySlug });
+
+      if (!category) {
+          return res.status(404).send({
+              success: false,
+              message: 'Category not found'
+          });
+      }
+
+      let product = await productModel.findOne({ 
+          slug: req.params.productSlug, 
+          category: category._id,
+          isDeleted: false 
+      });
+
+      if (!product) {
+          return res.status(404).send({
+              success: false,
+              message: 'Product not found'
+          });
+      }
+
+      res.status(200).send({
+          success: true,
+          data: product
+      });
+  } catch (error) {
+      res.status(500).send({
+          success: false,
+          message: error.message
+      });
+  }
+});
 
 module.exports = router;
