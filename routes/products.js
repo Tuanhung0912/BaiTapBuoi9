c8b872c9b57a6617520195098224da61aed8bc2c
tuanhung0912 <tuanhung.bh2003@gmail.com>
2025-04-03 14:32:22 +0700

    Fix Wrong Code

--- routes/products.js
+++ routes/products.js
@@ -59,32 +59,6 @@ router.get('/:id', async function(req, res, next) {
   }
 });
 
-// Hiển thị tất cả sản phẩm trong một danh mục bằng slug của danh mục
-router.get('/slug/:categorySlug', async function(req, res, next) {
-  try {
-      let category = await categoryModel.findOne({ slug: req.params.categorySlug });
-
-      if (!category) {
-          return res.status(404).send({
-              success: false,
-              message: 'Category not found'
-          });
-      }
-
-      let products = await productModel.find({ category: category._id, isDeleted: false });
-      res.status(200).send({
-          success: true,
-          data: products
-      });
-  } catch (error) {
-      res.status(500).send({
-          success: false,
-          message: error.message
-      });
-  }
-});
-
-// Hiển thị một sản phẩm theo slug trong một danh mục
 router.get('/slug/:categorySlug/:productSlug', async function(req, res, next) {
   try {
       let category = await categoryModel.findOne({ slug: req.params.categorySlug });
@@ -121,32 +95,7 @@ router.get('/slug/:categorySlug/:productSlug', async function(req, res, next) {
   }
 });
 
-// Hiển thị sản phẩm trong danh mục bằng `slug` của danh mục
-router.get('/slug/:categorySlug', async function(req, res, next) {
-  try {
-      let category = await categoryModel.findOne({ slug: req.params.categorySlug });
 
-      if (!category) {
-          return res.status(404).send({
-              success: false,
-              message: 'Category not found'
-          });
-      }
-
-      // Tìm các sản phẩm trong danh mục này
-      let products = await productModel.find({ category: category._id, isDeleted: false });
-
-      res.status(200).send({
-          success: true,
-          data: products
-      });
-  } catch (error) {
-      res.status(500).send({
-          success: false,
-          message: error.message
-      });
-  }
-});
 
 router.post('/', check_authentication, check_authorization(['mod']), async function(req, res, next) {
   try {
